9f62bf40d6e55e0c5f199741b3dc7eb225a7d632
Aviad Roichman <aviadroichman@gmail.com>
2024-08-28 12:38:41 +0300

    creating the backend

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, jsonify
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+
+load_dotenv()
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        dbname=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT')
+    )
+    return conn
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:5500","http://127.0.0.1:5501"]}})
+
+
+@app.route('/', methods=['GET'])
+def show_books():
+    conn = get_db_connection()
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    cur.execute('SELECT * FROM books;')
+    books = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(books)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
